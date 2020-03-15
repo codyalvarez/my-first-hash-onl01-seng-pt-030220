be17da684532103c827a3108a501e9f833dd213f
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-03-15 18:32:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,8 +23,6 @@ shipping_manifest << {
   "muskets" => 2,
   "gun powder" => 4
 }
-
-
 # add 2 muskets to the shipping_manifest hash below
 # add 4 gun powder to the shipping_manifest hash below
 # return the shipping_manifest hash below
